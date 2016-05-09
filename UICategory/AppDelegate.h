1b6836a38219137db55d8d571d8f35c8475d953c
acekiller <fantasy028@sina.com>
2016-05-09 13:28:22 +0800

    create new proj

---
+++ UICategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICategory
+//
+//  Created by fengxijun on 5/9/16.
+//  Copyright © 2016 goappbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UICategory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UICategory
+//
+//  Created by fengxijun on 5/9/16.
+//  Copyright © 2016 goappbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
